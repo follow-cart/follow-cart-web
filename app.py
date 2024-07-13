63d77aab8ec3c088b0a187dfdf02f0244c57df77
BLUEVERY8 <bluevery8@naver.com>
2024-07-13 16:48:36 +0900

    로봇 실시간 위치 미세하게 이동

--- app.py
+++ app.py
@@ -1,56 +1,46 @@
 from flask import Flask, Response, render_template, request, jsonify
-import cv2
-import numpy as np
-from ultralytics import YOLO
-import rclpy
-from rclpy.node import Node
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
 import roslibpy
 
 # Flask 애플리케이션 설정
 app = Flask(__name__, static_url_path='/static')
 
-# YOLOv8 모델 로드
-# model = YOLO('./resource/yolov8n.pt')
-#
-# # ROSBridge 연결 설정
-# ros = roslibpy.Ros(host='localhost', port=9090)
-# ros.run()
-#
-# # Publisher 설정
-# convoy_topic = roslibpy.Topic(ros, '/convoy_cmd', 'std_msgs/String')
-# pedestrian_topic = roslibpy.Topic(ros, '/pedestrian_cmd', 'std_msgs/String')
-#
-# def move_pedestrian(direction):
-#     cmd = roslibpy.Message({
-#         'data': ''
-#     })
-#
-#     if direction == 'forward':
-#         cmd['data'] = 'Forward'
-#     elif direction == 'backward':
-#         cmd['data'] = 'Backward'
-#     elif direction == 'left':
-#         cmd['data'] = 'Left Turn'
-#     elif direction == 'right':
-#         cmd['data'] = 'Right Turn'
-#     elif direction == 'stop':
-#         cmd['data'] = 'Stop'
-#
-#     pedestrian_topic.publish(cmd)
-#
-# def execute_cmd(cmd):
-#     cmd = roslibpy.Message({
-#         'data': ''
-#     })
-#
-#     if cmd == 'call':
-#         cmd['data'] = 'Call'
-#     elif cmd == 'return':
-#         cmd['data'] = 'Return'
-#
-#     pedestrian_topic.publish(cmd)
+# ROSBridge 연결 설정
+ros = roslibpy.Ros(host='localhost', port=9090)
+ros.run()
+
+# Publisher 설정
+convoy_topic = roslibpy.Topic(ros, '/convoy_cmd', 'std_msgs/String')
+pedestrian_topic = roslibpy.Topic(ros, '/pedestrian_cmd', 'std_msgs/String')
+
+def move_pedestrian(direction):
+    msg = roslibpy.Message({
+        'data': ''
+    })
+
+    if direction == 'forward':
+        msg['data'] = 'Forward'
+    elif direction == 'backward':
+        msg['data'] = 'Backward'
+    elif direction == 'left':
+        msg['data'] = 'Left Turn'
+    elif direction == 'right':
+        msg['data'] = 'Right Turn'
+    elif direction == 'stop':
+        msg['data'] = 'Stop'
+
+    pedestrian_topic.publish(msg)
+
+def execute_cmd(cmd):
+    msg = roslibpy.Message({
+        'data': ''
+    })
+
+    if cmd == 'call':
+        msg['data'] = 'Call'
+    elif cmd == 'return':
+        msg['data'] = 'Return'
+
+    convoy_topic.publish(msg)
 
 
 @app.route('/')
@@ -58,24 +48,24 @@ def index():
     return render_template('index.html')
 
 
-# @app.route('/move', methods=['POST'])
-# def move():
-#     direction = request.json.get('direction')
-#     if direction in ['forward', 'backward', 'left', 'right', 'stop']:
-#         move_pedestrian(direction)
-#         return jsonify({"status": "success", "message": f"Moving {direction}"}), 200
-#     else:
-#         return jsonify({"status": "error", "message": "Invalid direction"}), 400
-#
-#
-# @app.route('/command', methods=['POST'])
-# def command():
-#     cmd = request.json.get('cmd')
-#     if cmd in ['call', 'return']:
-#         execute_cmd(cmd)
-#         return jsonify({"status": "success", "message": f"Executing {cmd}"}), 200
-#     else:
-#         return jsonify({"status": "error", "message": "Invalid command"}), 400
+@app.route('/move', methods=['POST'])
+def move():
+    direction = request.json.get('direction')
+    if direction in ['forward', 'backward', 'left', 'right', 'stop']:
+        move_pedestrian(direction)
+        return jsonify({"status": "success", "message": f"Moving {direction}"}), 200
+    else:
+        return jsonify({"status": "error", "message": "Invalid direction"}), 400
+
+
+@app.route('/command', methods=['POST'])
+def command():
+    cmd = request.json.get('command')
+    if cmd in ['call', 'return']:
+        execute_cmd(cmd)
+        return jsonify({"status": "success", "message": f"Executing {cmd}"}), 200
+    else:
+        return jsonify({"status": "error", "message": "Invalid command"}), 400
 
 
 if __name__ == '__main__':
